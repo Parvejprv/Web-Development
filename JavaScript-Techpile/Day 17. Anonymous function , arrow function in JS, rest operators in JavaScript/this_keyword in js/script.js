3ecc167a164b3d95ee6d33ba45603db803ac17bd
Parvej Alam <parvejprv1@gmail.com>
2024-12-28 14:04:13 +0530

    this keyword in Java-Script

---
+++ JavaScript-Techpile/Day 17. Anonymous function , arrow function in JS, rest operators in JavaScript/this_keyword in js/script.js
@@ -0,0 +1,17 @@
+/*  the given below method gives the current object */
+
+/* 
+let demo = (x) => {
+  alert(x);
+} 
+*/
+
+
+/* this method will get the current object value */
+let demo = (x) => {
+  alert(x.value);
+  x.style.backgroundColor = "red";
+  x.style.border = "10px groove green";
+}
+
+
